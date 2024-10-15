deec02cc2f51f4cba01d67e7f61bd8f7609eec4e
Prakash-b1 <pinnub23@gmail.com>
2024-10-15 22:21:58 +0530

    changes

--- src/Pages/A70/components/Section4Securitypage.jsx
+++ src/Pages/A70/components/Section4Securitypage.jsx
@@ -1,14 +1,10 @@
 import GradientBackgroundWrapper from "../../../components/GradientBackgroundWrapper "
-import sec4Img from "../../../assets/A70assets/touchid.svg"
-import facescan from "../../../assets/A70assets/facescanid.svg"
+import sec4Img from "../../../assets/A70assets/image.png"
 const Section4Securitypage = () => {
     return (
         <GradientBackgroundWrapper>
             <div className='relative flex flex-col-reverse xl:flex-col gap-5  xl:h-[739px] xl:w-[1248px] overflow-hidden'>
-                <div className="absolute xl:top-[9rem]  xl:left-[15rem] top-[208px] left-[2rem] md:top-[14rem] md:left-[8rem] z-10">
-                    <img src={facescan} alt="facescan" className="w-[32px] md:w-[218px] md:h-[250px] xl:h-[311px] " />
-
-                </div>
+                
 
                 <img src={sec4Img} alt="bgimgsec3" className="xl:absolute top-[20rem]  xl:h-[739px] xl:w-[1248px] md:top-0 left-0 w-full  object-cover" />
                 <div className="z-10 flex xl:justify-end justify-center text-center xl:text-left">
